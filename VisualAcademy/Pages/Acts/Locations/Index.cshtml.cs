fae63de213b5e86953c066aecd484fa8c89e976f
Park YongJune <redplus@redplus.net>
2022-03-04 02:36:46 +0900

    Location CRUD

---
+++ VisualAcademy/Pages/Acts/Locations/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VisualAcademy.Data;
+using VisualAcademy.Models;
+
+namespace VisualAcademy.Pages.Acts.Locations
+{
+    public class IndexModel : PageModel
+    {
+        private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+        public IndexModel(VisualAcademy.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Location> Location { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Location = await _context.locations
+                .Include(l => l.PropertyRef).ToListAsync();
+        }
+    }
+}
